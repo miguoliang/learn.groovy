705eb92b928c31f161c5863f3f563131fb06d5a2
nothingmi <nothingmi@muchencute.com>
2016-09-03 23:07:46 +0800

    Add MapSection in main()

--- src/com/muchencute/learn/groovy/Tutorial.groovy
+++ src/com/muchencute/learn/groovy/Tutorial.groovy
@@ -1,9 +1,6 @@
 package com.muchencute.learn.groovy
 
-import com.muchencute.learn.groovy.sections.ArraySection
-import com.muchencute.learn.groovy.sections.FileSection
-import com.muchencute.learn.groovy.sections.MathSection
-import com.muchencute.learn.groovy.sections.StringSection
+import com.muchencute.learn.groovy.sections.*
 
 class Tutorial {
 
@@ -15,6 +12,7 @@ class Tutorial {
         new StringSection().run();
         new FileSection().run();
         new ArraySection().run();
+        new MapSection().run();
     }
 
 }
